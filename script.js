df51ce9181ef58392b6e43cccfb2fd347f766749
Muhammad Khabbaz <muhammad.khabbaz@outlook.com>
2023-09-03 14:01:35 +0700

    cleaned up code

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ let numbers = ["1","2","3","4","5","6","7","8","9","0"]
 let special = ["!","@","#","$","%","*","-","_","~"]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+//assembling password properties
 function generatePassword() {
-//confirm length
+  //confirm length
   let numChars = Number(window.prompt("how manycharacters do you want your password to be 8-128")); 
   while(true){
     if(numChars < 8 || numChars > 128){
@@ -17,54 +18,53 @@ function generatePassword() {
     break;
     }
   }
-//list of possible characters in password
+  //creating list of possible characters in password
   while(true){  
     let passchar = [];
-//confirm upercase letters
-  if(window.confirm("Do you want to use uppercase letters")){
+    //confirm upercase letters
+    if(window.confirm("Do you want to use uppercase letters")){
       window.alert("uppercase letters will be included");
       passchar = passchar.concat(...uppercase);
-  }else{
-    window.alert("uppercase letters will not be included");
-  }
-//confirm lowercase letters
-  if(window.confirm("Do you want to use lowercase letters")){
-    window.alert("lowercase letters will be included");
-    passchar = passchar.concat(...lowercase);
-  }else{
-    window.alert("lowercase letters will not be included");
-  }
-//confirm numbers
-  if(window.confirm("Do you want to use numbers")){
-    window.alert("numbers will be included");
-    passchar = passchar.concat(...numbers);
-  }else{
-    window.alert("numbers will not be included");
-  }
-//confirm special characters
-  if(window.confirm("do you want to use special characters")){
-    window.alert("special characters will be used");
-    passchar = passchar.concat(...special);
-  }else{
-    window.alert("special characters will not be used");
-  }
-//if list is empty
-if(passchar.length>0){
-let password = [];
-    for(let i = 0; i <= numChars; i++ )
-    {
-      let idx = Math.floor(Math.random()*passchar.length);
-      let pwdChar = passchar[idx];
-      password.push(pwdChar);
+    }else{
+      window.alert("uppercase letters will not be included");
     }
-    console.log(password);
-    return password.join('');
-  }else
-    window.alert("You Must Chose an Option")
-    continue
-}
-//generating password
+    //confirm lowercase letters
+    if(window.confirm("Do you want to use lowercase letters")){
+      window.alert("lowercase letters will be included");
+      passchar = passchar.concat(...lowercase);
+    }else{
+      window.alert("lowercase letters will not be included");
+    }
+    //confirm numbers
+    if(window.confirm("Do you want to use numbers")){
+      window.alert("numbers will be included");
+      passchar = passchar.concat(...numbers);
+    }else{
+      window.alert("numbers will not be included");
+    }
+    //confirm special characters
+    if(window.confirm("do you want to use special characters")){
+      window.alert("special characters will be used");
+      passchar = passchar.concat(...special);
+    }else{
+    window.alert("special characters will not be used");
+    }
     
+    if(passchar.length>0){
+      //generating password
+      let password = [];
+      for(let i = 0; i <= numChars; i++ ){
+        let idx = Math.floor(Math.random()*passchar.length);
+        let pwdChar = passchar[idx];
+        password.push(pwdChar);
+        return password.join('');
+      }
+      // if password list is empty
+      }else{
+        window.alert("You Must Chose an Option")
+        continue
+      }
+  }
 }
 // Write password to the #password input
 function writePassword() {
