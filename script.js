6868bd8f46fdf5c421755c2daae10c84d3800eb8
MuhammadKhabbaz <141904926+MuhammadKhabbaz@users.noreply.github.com>
2023-09-04 12:21:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const lowercase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
 const uppercase = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
 const numbers = ["1","2","3","4","5","6","7","8","9","0"]
-const special = ["!","@","#","$","%","*","-","_","~"]
+const special = ["!","@","#","$","%","*","-","_","~","^","&","(",")"]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 //assembling password properties
